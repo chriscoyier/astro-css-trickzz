28fcb18812c3b886c4bd5013d95e255cdc0b1fc5
Chris Coyier <chriscoyier@gmail.com>
2021-06-29 16:29:30 -0700

    dynamic component

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,19 +1,36 @@
-<template>
-  <footer>{{ greeting }} World!</footer>
-</template>
-
 <script>
+import { ref } from 'vue';
 export default {
   setup() {
+    const count = ref(0);
+    const add = () => (count.value = count.value + 1);
+    const subtract = () => (count.value = count.value - 1);
     return {
-      greeting: 'Hello'
+      count,
+      add,
+      subtract
     };
   }
 };
 </script>
 
-<style scoped>
+<template>
+  <footer>
+    <button @click="subtract()">-</button>
+    {{ count }}
+    <button @click="add()">+</button>
+  </footer>
+</template>
+
+<style scoped lang="scss">
 footer {
+  padding: 1rem;
+  color: white;
+  display: flex;
+  justify-content: center;
   background: linear-gradient(130deg, #ff7a18, #af002d 41.07%, #319197 76.05%);
+  > * {
+    margin-inline: 1rem;
+  }
 }
 </style>
